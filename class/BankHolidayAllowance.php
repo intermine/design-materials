9415e8e4447ef47e67d825cef4caa75fc136fda3
Radek Stepan <radek.stepan@gmail.com>
2011-10-03 12:59:37 +0100

    editing bank holiday allowance

--- class/BankHolidayAllowance.php
+++ class/BankHolidayAllowance.php
@@ -1,7 +1,4 @@
 <?php
-
-require_once "db.inc.php";
-
 /**
  * User: radek
  * Date: 30/09/11
@@ -9,6 +6,9 @@ require_once "db.inc.php";
  *
  * Used for fetching and saving bank holiday allowance of peeps
  */
+
+require_once "db.inc.php";
+require_once "class/Messaging.php";
 
 class BankHolidayAllowance {
 
@@ -19,6 +19,7 @@ class BankHolidayAllowance {
     function __construct() {
         $this->today = getdate();
         $this->year = $this->today['year'];
+        $this->getAllowance();
     }
 
     /**
@@ -26,17 +27,67 @@ class BankHolidayAllowance {
      */
     function getAllowance() {
         if ($this->allowance == NULL) {
-            $res = Db::getConnection()->query(sprintf("SELECT * FROM `allowance` WHERE ayear BETWEEN %d AND %d ORDER BY userid ASC",
-                                                      $this->year-1, $this->year+1));
+            // fetch all active users (not 'deleted')
+            $res = Db::getConnection()->query("SELECT * FROM `worker` WHERE deleted='N' ORDER BY fullname ASC");
+
             $this->allowance = array();
             while ($row = $res->fetchArray()) {
                 if (!$this->allowance[$row['userid']]) {
-                    $this->allowance[$row['userid']] = array($this->year-1 => '-', $this->year => '-', $this->year+1 => '-');
+                    $this->allowance[$row['userid']] = array('name' => $row['fullname'],
+                                                             'allowance' => array(
+                                                                 $this->year-1 => 0,
+                                                                 $this->year => 0,
+                                                                 $this->year+1 => 0));
                 }
-                $this->allowance[$row['userid']][$row['ayear']] = (float) $row['allowance'];
+            }
+
+            // fetch from allowance table
+            $res = Db::getConnection()->query(sprintf("SELECT * FROM `allowance` WHERE ayear BETWEEN %d AND %d ORDER BY userid ASC",
+                                                      $this->year-1, $this->year+1));
+            // merge the tables together
+            while ($row = $res->fetchArray()) {
+                if ($this->allowance[$row['userid']]) {
+                    $this->allowance[$row['userid']]['allowance'][$row['ayear']] = (float) $row['allowance'];
+                }
             }
         }
         return $this->allowance;
     }
+
+    /**
+     * Update the allowance per person/year
+     * @param  $post
+     * @return void
+     */
+    function updateAllowance($post) {
+        // traverse all userid's
+        foreach ($post as $userid => $map) {
+            foreach ($map as $year => $allowance) {
+                $allowance = (float) $allowance;
+                // does the allowance differ from what we have?
+                if ($this->allowance[$userid]['allowance'][$year] != $allowance) {
+                    // db updates
+                    if (Db::getConnection()->query(sprintf(
+                                                       "SELECT allowance FROM allowance WHERE userid='%s' AND ayear=%d",
+                                                       $userid, $year))->numRows() != 0) {
+                        // update
+                        Db::getConnection()->query(sprintf(
+                        "UPDATE `allowance` SET allowance=%f WHERE userid='%s' AND ayear=%d", $allowance, $userid, $year));
+                    } else {
+                        // add
+                        Db::getConnection()->query(sprintf(
+                        "INSERT INTO `allowance` (userid,ayear,allowance) VALUES ('%s',%d,%f)", $userid, $year, $allowance));
+                    }
+                    Messaging::ok(
+                        sprintf("Allowance for %s in %d set to %s", $this->allowance[$userid]['name'], $year, round($allowance, 1))
+                    );
+
+                    // internal update
+                    $this->allowance[$userid]['allowance'][$year] = $allowance;
+                }
+            }
+        }
+    }
+
 }
 
--- users_allowance.php
+++ users_allowance.php
@@ -24,30 +24,48 @@ Page::init(FALSE);
 // load the existing allowance of peeps
 require_once('class/BankHolidayAllowance.php');
 $bha = new BankHolidayAllowance();
+
+// on post
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bha->updateAllowance($_POST);
+
+    # any messages?
+    require_once "class/Messaging.php";
+    $messages = Messaging::get();
+    if ($messages) {
+        echo '<ul id="messages">';
+        foreach ($messages as $message) {
+            printf("<li class='%s'>%s</li>", $message['type'], $message['text']);
+        }
+        echo '</ul>';
+    }
+}
+
 ?>
 
 <center>
 <form method="post">
-<table>
+<table id="edit-planner">
     <thead>
         <tr>
-            <th>userid</th>
+            <td></td>
             <th><?php echo $bha->year-1; ?></th>
             <th><?php echo $bha->year; ?></th>
             <th><?php echo $bha->year+1; ?></th>
         </tr>
     </thead>
     <tbody>
-    <?php foreach ($bha->getAllowance() as $userid => $years) { ?>
+    <?php foreach ($bha->getAllowance() as $userid => $map) { ?>
     <tr>
-        <td><?php echo $userid; ?></td>
-        <?php foreach ($years as $year => $allowance) { ?>
-            <td><?php echo $allowance; ?></td>
+        <td><?php echo $map['name']; ?></td>
+        <?php foreach ($map['allowance'] as $year => $allowance) { ?>
+            <td><input type="text" name="<?php echo $userid.'['.$year.']'; ?>" value="<?php echo $allowance; ?>" /></td>
         <?php } ?>
     </tr>
     <?php } ?>
     </tbody>
 </table>
+<input type="submit" value="Save">
 </form>
 </center>
 
