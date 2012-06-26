075b71f33040461c810e466b08e5873f382626a3
Radek Stepan <dev@radekstepan.com>
2012-06-26 15:03:11 +0100

    bump theme number and change url

--- websites/intermine.org/setup.py
+++ websites/intermine.org/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'TracInterMineTheme',
-    version = '2.0',
+    version = '2.0.1',
     packages = ['interminetheme'],
     package_data = { 'interminetheme': ['templates/*.html', 'htdocs/*.css', 'htdocs/favicon.ico', 'htdocs/img/*.png', 'htdocs/img/*.gif', 'htdocs/img/*.jpg' ] },
 
@@ -14,7 +14,7 @@ setup(
     description = 'An InterMine 2.0 Trac site theme (end of 2011).',
     license = 'BSD',
     keywords = 'trac plugin theme',
-    url = 'https://github.com/radekstepan/InterMine-Trac-Theme',
+    url = 'https://github.com/intermine/design-materials/tree/master/websites/intermine.org',
     classifiers = [
         'Framework :: Trac',
     ],
